2fd668f1fe8a1056bf035285370a7a1110d62746
dni <office@dnilabs.com>
2026-06-23 06:52:25 +0200

    dynamically update blockexplorer compoenent with websockets

--- lnbits/static/i18n/en.js
+++ lnbits/static/i18n/en.js
@@ -929,6 +929,7 @@ window.localisation.en = {
   inputs: 'Inputs',
   outputs: 'Outputs',
   confirmations: 'Confirmations',
+  confirmed: 'Confirmed',
   unconfirmed: 'Unconfirmed',
   no_transactions: 'No transactions found',
   address: 'Address'
